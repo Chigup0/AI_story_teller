94bd8b00f901b3326cbe5acce5a7849123f2e368
Chigup0 <chiraggupta56565656@gmail.com>
2025-08-19 22:18:45 +0530

    signup

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -5,13 +5,37 @@ import "./Auth.css";
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
   const navigate = useNavigate();
 
   const handleSignup = (e) => {
     e.preventDefault();
     // TODO: Call backend API for signup
     console.log("Signing up with", { email, password });
-    navigate("/"); // redirect to home/chat
+    navigate("/"); 
+    try {
+    const res = await fetch("http://127.0.0.1:8000/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username,email, password }),
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      console.log("Signup success:", data);
+
+      // Redirect to login page
+      navigate("/login");
+    } else {
+      const err = await res.json();
+      alert("Signup failed: " + err.detail);
+    }
+  } catch (err) {
+    console.error("Error during signup:", err);
+  }
+};
+
+    // redirect to home/chat
   };
 
   return (
@@ -23,6 +47,13 @@ export default function Signup() {
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+         <input
+          type="username"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
           required
         />
         <input
